ac3b199197be589089f425d3de92c1ec91c135e3
Robergwillian <roberg.willian@gmail.com>
2021-11-17 15:08:31 -0300

    css ajustments

--- src/containers/Header/styles.js
+++ src/containers/Header/styles.js
@@ -7,4 +7,5 @@ export const HeaderWrapper = styled.header`
     margin:0 auto;
     background-image:  url(${headerbg}), linear-gradient(314.72deg, #C86DD7 -1.5%, #7E49C3 39.43%, #5912AC 86.02%);
     background-repeat: no-repeat;
+    background-position:center;
 `;
